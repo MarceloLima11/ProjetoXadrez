2ef28a815397ceb105d4317315dd0c3063b71f6a
Marcelo Lima <marcelolima152021@gmail.com>
2021-07-21 10:44:42 -0300

    Criando a classe Posicao

--- ex 088/Program.cs
+++ ex 088/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace ex_088
 {
@@ -6,7 +7,9 @@ namespace ex_088
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
